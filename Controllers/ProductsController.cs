8ab3ba36d594f23d964f75db54354d9081a96761
aditya-772 <adityach772@gmail.com>
2024-11-13 11:58:56 -0600

    Initial Commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Feature.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Feature.Data;
+
+namespace Feature.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Feature.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String ProductName {  get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
